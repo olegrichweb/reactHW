dce79fc465da1b38d02fc61e4e0b695fbc661376
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-04-26 18:00:50 +0300

    Updates

---
+++ precache-manifest.130e474df183b3ebd6584a24b43fef2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7c5fe025ecdd3f4b7041499a1b1455",
+    "url": "/reactHW/index.html"
+  },
+  {
+    "revision": "59aa0dd3ee0084704d12",
+    "url": "/reactHW/static/css/main.bca79f53.chunk.css"
+  },
+  {
+    "revision": "ee6995ab5f31feec15dd",
+    "url": "/reactHW/static/js/2.b44c012e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactHW/static/js/2.b44c012e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59aa0dd3ee0084704d12",
+    "url": "/reactHW/static/js/main.77bc6769.chunk.js"
+  },
+  {
+    "revision": "6ad0ec75b2c4bf6cb39a",
+    "url": "/reactHW/static/js/runtime-main.331d2b71.js"
+  }
+]);
